219149182f48efd980d88abe6bac267dd17b862f
jovv <jochen.vandevoorde@ordina.be>
2018-04-21 07:52:23 +0200

    Add chapter 1 exercises

---
+++ src/main/scala/Chapter1.scala
@@ -0,0 +1,10 @@
+object Chapter1 {
+  def main(args: Array[String]): Unit = {
+    println(3.toString)
+    val s = math.sqrt(3)
+    val s2 = math.sqrt(s)
+    println(s, s2)
+    10 max 2
+    val b = BigInt(2)
+  }
+}
